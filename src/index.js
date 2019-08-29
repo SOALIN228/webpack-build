c4d9831e16f09619c25faca86ec56153e45c89ea
lml <soalin228@163.com>
2019-08-29 17:26:08 +0800

    添加file-loader和url-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello')
+import th from './th.jpg'
+
+var img = new Image()
+img.src = th
+
+var root = document.getElementById('root')
+root.append(img)
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,21 @@ module.exports = {
   entry: { // 入口文件
     main: './src/index.js' // 文件名为main.js
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name]_[hash].[ext]', // 配置打包后的名字 ext为文件扩展名
+            outputPath: 'images/', // 输出路径
+            limit: 20480 // 图片大于2kb使用base64进行打包，减少http请求
+          }
+        }
+      }
+    ]
+  },
   output: { // 出口文件
     filename: 'bundle.js', // 输出文件名
     path: path.resolve(__dirname, 'dist') // 输出文件路径 __dirname为webpack.config当前文件
